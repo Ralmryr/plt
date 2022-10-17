3d7f335c359c9388511da977a53ea0247b056669
AlexLecl <alexandre.leclercq@ensea.fr>
2022-10-17 15:46:42 +0200

    Revert "Merge branch 'dev-alex' into pre-merge"
    This reverts commit 510088c83438aede5744195e51c4d81d37c164c5.

---
+++ cmake-build-debug/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/alex/Documents/3A/projet/plt/test
+# Build directory: /home/alex/Documents/3A/projet/plt/cmake-build-debug/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("shared")
+subdirs("client")

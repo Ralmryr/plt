9e3dd2b27f72aa48f73e1e8208d3ac1a1efbfce5
AlexLecl <alexandre.leclercq@ensea.fr>
2022-10-17 15:39:26 +0200

    update .gitignore

--- cmake-build-debug/run_test.sh
+++
@@ -1,2 +0,0 @@
-cd "/home/alex/Documents/3A/projet/plt/cmake-build-debug/test"
-ctest --timeout 300 ||/bin/true
--- cmake-build-debug/test/shared/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alex/Documents/3A/projet/plt/test/shared
-# Build directory: /home/alex/Documents/3A/projet/plt/cmake-build-debug/test/shared
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_shared_dummy "/home/alex/Documents/3A/projet/plt/cmake-build-debug/test/shared/test_shared_dummy")
-set_tests_properties(test_shared_dummy PROPERTIES  WORKING_DIRECTORY "/home/alex/Documents/3A/projet/plt" _BACKTRACE_TRIPLES "/home/alex/Documents/3A/projet/plt/test/CMakeLists.txt;77;add_test;/home/alex/Documents/3A/projet/plt/test/shared/CMakeLists.txt;2;add_custom_test;/home/alex/Documents/3A/projet/plt/test/shared/CMakeLists.txt;0;")

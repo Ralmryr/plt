bced0ab44f30cf8112e56603fdbddcf62557ac01
AlexLecl <alexandre.leclercq@ensea.fr>
2022-10-24 16:14:54 +0200

    remove debug files

--- cmake-build-debug/run_test.sh
+++
@@ -1,6 +0,0 @@
-<<<<<<< HEAD
-cd "/home/alex/Documents/3A/projet/plt/cmake-build-debug/test"
-=======
-cd "/home/ismail/Documents/PLT/plt/cmake-build-debug/test"
->>>>>>> dev-isma
-ctest --timeout 300 ||/bin/true

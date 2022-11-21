1aed541903e5d3ddfea124544b41f8b50d4a49fb
AlexLecl <alexandre.leclercq@ensea.fr>
2022-11-21 19:21:38 +0100

    unique vector to shared

--- src/client/render/StdProjectDisplay.cpp
+++ src/client/render/StdProjectDisplay.cpp
@@ -9,56 +9,56 @@ StdProjectDisplay::StdProjectDisplay() {
     sf::Vector2f posFrameImage;
     posFrameImage.x=0;
     posFrameImage.y=0;
-    this->frameImage = Image("../src/client/img/std_project_background",posFrameImage);
-    listComponents.push_back(make_unique<Image>(frameImage));
+    this->frameImage = make_shared<Image>(Image("../src/client/img/std_project_background",posFrameImage));
+    listComponents.push_back(frameImage);
 
     //discardButton
     sf::Vector2f posDiscardButton;
     posFrameImage.x=0;
     posFrameImage.y=0;
     ClickableArea clickAreaDiscardButton=ClickableArea();
-    this->discardButton=Button("../src/client/img/std_project_discard", clickAreaDiscardButton,posDiscardButton);
-    listComponents.push_back(make_unique<Button>(discardButton));
+    this->discardButton=make_shared<Button>(Button("../src/client/img/std_project_discard", clickAreaDiscardButton,posDiscardButton));
+    listComponents.push_back(discardButton);
 
     //prodEnergyButton
     sf::Vector2f posProdEnergyButton;
     posProdEnergyButton.x=0;
     posProdEnergyButton.y=0;
     ClickableArea clickAreaProdEnergyButton=ClickableArea();
-    this->discardButton=Button("../src/client/img/std_project_prod_energy", clickAreaProdEnergyButton,posProdEnergyButton);
-    listComponents.push_back(make_unique<Button>(prodEnergyButton));
+    this->prodEnergyButton=make_shared<Button>(Button("../src/client/img/std_project_prod_energy", clickAreaProdEnergyButton,posProdEnergyButton));
+    listComponents.push_back(prodEnergyButton);
 
     //temperatureButton
     sf::Vector2f posTemperatureButton;
     posTemperatureButton.x=0;
     posTemperatureButton.y=0;
     ClickableArea clickAreaTemperatureButton=ClickableArea();
-    this->discardButton=Button("../src/client/img/std_project_temperature", clickAreaTemperatureButton,posTemperatureButton);
-    listComponents.push_back(make_unique<Button>(temperatureButton));
+    this->temperatureButton=make_shared<Button>(Button("../src/client/img/std_project_temperature", clickAreaTemperatureButton,posTemperatureButton));
+    listComponents.push_back(temperatureButton);
 
     //oceanButton
     sf::Vector2f posOceanButton;
     posOceanButton.x=0;
     posOceanButton.y=0;
     ClickableArea clickAreaOceanButton=ClickableArea();
-    this->discardButton=Button("../src/client/img/std_project_ocean", clickAreaOceanButton,posOceanButton);
-    listComponents.push_back(make_unique<Button>(oceanButton));
+    this->oceanButton=make_shared<Button>(Button("../src/client/img/std_project_ocean", clickAreaOceanButton,posOceanButton));
+    listComponents.push_back(oceanButton));
 
     //forestButton
     sf::Vector2f posForestButton;
     posForestButton.x=0;
     posForestButton.y=0;
     ClickableArea clickAreaForestButton=ClickableArea();
-    this->discardButton=Button("../src/client/img/std_project_forest", clickAreaForestButton,posForestButton);
-    listComponents.push_back(make_unique<Button>(forestButton));
+    this->forestButton= make_shared<Button>(Button("../src/client/img/std_project_forest", clickAreaForestButton,posForestButton));
+    listComponents.push_back(forestButton);
 
     //cityButton
     sf::Vector2f posCityButton;
     posCityButton.x=0;
     posCityButton.y=0;
     ClickableArea clickAreaCityButton=ClickableArea();
-    this->discardButton=Button("../src/client/img/std_project_city", clickAreaCityButton,posCityButton);
-    listComponents.push_back(make_unique<Button>(cityButton));
+    this->cityButton=make_shared<Button>(Button("../src/client/img/std_project_city", clickAreaCityButton,posCityButton));
+    listComponents.push_back(cityButton);
 
 
 }
@@ -71,5 +71,5 @@ void StdProjectDisplay::draw(sf::RenderWindow &window) {
     }
 }
 
-void StdProjectDisplay::update() {
+void StdProjectDisplay::update(std::unordered_map<std::string,std::string> data) {
 }

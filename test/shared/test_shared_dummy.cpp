399daf57ef4286af1a09a5e3b1daefed0403597b
Ralmryr <clementdury2@gmail.com>
2023-01-29 00:12:17 +0100

    Added small test

--- test/shared/test_shared_dummy.cpp
+++ test/shared/test_shared_dummy.cpp
@@ -1,10 +1,17 @@
 
 #include <boost/test/unit_test.hpp>
-
+#include <state.h>
 
 BOOST_AUTO_TEST_CASE(TestStaticAssert)
 {
     BOOST_CHECK(1);
 }
 
-/* vim: set sw=2 sts=2 et : */
+BOOST_AUTO_TEST_CASE(TestState)
+{
+    {
+        using namespace state;
+        State state(5);
+        BOOST_CHECK_EQUAL(state.getCurrentPlayer()->getId(), 0);
+    }
+}
